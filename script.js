0e6fbb029a0a5afe0bb9649a64bdde28bcda248a
tucker1234456 <tkamalsky27@students.stab.org>
2024-02-28 11:19:52 -0500

    6.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var teamLocation= getColumn (url,2)
 var teamStadium = getColumn (url,3)
 var stadiumCapacity = getColumn (url,4)
 var teamConference =getColumn (url,7)
+var joined = getColumn (url,5)
 
 function getTeamsInConference(conference){
     var matches= [];
@@ -17,7 +18,41 @@ function getTeamsInConference(conference){
     if(matches.length ==0){
         matches.push("this conference doesn't exist")
     }
-   return matches;
+    return matches;
    
 }
-// console.log (getTeamsInConference("eastedfghrn")) ;
+//  console.log (getTeamsInConference("eastern")) ;
+
+ function getCity(team) {
+    var matches ="that team doesn't exist";
+    for (var i = 0; i < teamName.length; i++) {
+        if(teamName[i].toLowerCase().includes(team.toLowerCase())){
+            matches = teamLocation[i];
+        }
+
+    }
+   return matches;
+    
+}
+// console.log (getCity("charlotte FC"))
+
+function getOldestTeam(conference){
+    var oldest = 3000;
+    var matches = "that division doesn't exist";
+    for (var i = 0; i < joined.length; i++) {
+        if((conference[i].toLowerCase()== conference.toLowerCase() && joined [i] <= oldest)){
+            matches = teamName[i];
+            oldest = joined[i];
+        }
+        if (oldestteamName== ""){
+            oldestteamName=" that divison doesn't exist"
+        }
+    }
+   return matches;
+    
+
+}
+ console.log (getOldestTeam("Eastern"));
+
+
+
