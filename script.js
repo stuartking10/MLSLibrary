c23011344f41b145e4fbd10e7f02fa1cc700ccfd
tucker1234456 <tkamalsky27@students.stab.org>
2024-02-28 11:02:27 -0500

    5.

--- script.js
+++ script.js
@@ -5,5 +5,15 @@ var teamLocation= getColumn (url,2)
 var teamStadium = getColumn (url,3)
 var stadiumCapacity = getColumn (url,4)
 var teamConference =getColumn (url,7)
+console.log(teamConference)
+function getTeamsInConference(conference){
+    var matches= ["this conference doesn't exist"];
+    for (var i = 0; i < teamConference.length; i++) {
+        if(teamConference[i].toLowerCase().includes(conference.toLowerCase())){
+            matches.push(teamName[i]);
+        }
 
-
+    }
+   return matches;
+}
+console.log (getTeamsInConference("eastern")) ;
