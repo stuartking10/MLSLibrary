04db9ca780efff4d2dd4d488127a10a33d2cffb2
tucker1234456 <tkamalsky27@students.stab.org>
2024-02-28 10:46:30 -0500

    1.

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log "hello"
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/main/Sports/MLS%20Teams.csv"
+var teamName = (url,1)
+var teamLocation (url,2)
+var teamStadium (url,3)
+var stadiumCapacity (url,4)
