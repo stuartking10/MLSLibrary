19a7987ae543f7b92e40ecf82d6eb13b6f0d85ac
tucker1234456 <tkamalsky27@students.stab.org>
2024-02-28 10:48:50 -0500

    2.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/main/Sports/MLS%20Teams.csv"
-var teamName = (url,1)
-var teamLocation (url,2)
-var teamStadium (url,3)
-var stadiumCapacity (url,4)
+var teamName = getColumn(url,1)
+var teamLocation= getColumn (url,2)
+var teamStadium = getColumn (url,3)
+var stadiumCapacity = getColumn (url,4)
+var teamConference =getColumn (url,7)
